1e20ed1489fabe846890807c28b35074e5a193f2
wzym <wzym@yandex.ru>
2016-10-27 10:21:15 +0300

    Increased main window size on start.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,6 +13,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         this.primaryStage = primaryStage;
+        primaryStage.setHeight(400);
+        primaryStage.setWidth(600);
         FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
         root = loader.load();
         Controller controller = loader.getController();
